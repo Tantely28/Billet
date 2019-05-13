3ad8d6406f804d53254b8f61b843bc2af08d2986
Tantely28 <rakotonari280689@gmail.com>
2019-05-13 13:28:42 +0200

    1.0

---
+++ enregistrementbillet.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  lun. 13 mai 2019 à 13:26
+-- Version du serveur :  10.1.38-MariaDB
+-- Version de PHP :  7.1.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `enregistrementbillet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `billet`
+--
+
+CREATE TABLE `billet` (
+  `Laharana` int(11) NOT NULL,
+  `Anarana` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `paye` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
